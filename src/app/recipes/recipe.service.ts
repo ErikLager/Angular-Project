71c66ce0ed973bd3d5c7a16aeb039d746937bfc9
Erik <43432787+ErikLager@users.noreply.github.com>
2021-11-24 15:59:31 +0100

    Cource Project - Http

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,28 +8,34 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('A test recipe',
-            'this is just a random test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-            [
-                new Ingredient('goreio', 1),
-                new Ingredient('noihåry', 6),
-                new Ingredient('fhenwu9', 2),
-                new Ingredient('jeiocn', 8),
-            ]),
-        new Recipe('A another test recipe',
-            'this is just a random test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-            [
-                new Ingredient('ngnrnbr', 1),
-                new Ingredient('greni', 6),
-                new Ingredient('grhbeui', 2),
-                new Ingredient('Meaning of life', 42),
-            ]),
-    ];
+    private recipes: Recipe[] = [];
+    // private recipes: Recipe[] = [
+    //     new Recipe('A test recipe',
+    //         'this is just a random test',
+    //         'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    //         [
+    //             new Ingredient('goreio', 1),
+    //             new Ingredient('noihåry', 6),
+    //             new Ingredient('fhenwu9', 2),
+    //             new Ingredient('jeiocn', 8),
+    //         ]),
+    //     new Recipe('A another test recipe',
+    //         'this is just a random test',
+    //         'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    //         [
+    //             new Ingredient('ngnrnbr', 1),
+    //             new Ingredient('greni', 6),
+    //             new Ingredient('grhbeui', 2),
+    //             new Ingredient('Meaning of life', 42),
+    //         ]),
+    // ];
 
     constructor(private shoppingService: ShoppingListService){}
+
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
